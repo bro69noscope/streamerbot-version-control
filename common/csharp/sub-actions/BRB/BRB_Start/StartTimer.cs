b3b47f9ef85ca51c7d02b962c09a9967f52a7419
Loïc Villet <villet.loic@gmail.com>
2026-07-25 19:05:19 +0200

    (c#, brb): fix type mistach for BrbManager.Start

--- common/csharp/sub-actions/BRB/BRB_Start/StartTimer.cs
+++ common/csharp/sub-actions/BRB/BRB_Start/StartTimer.cs
@@ -1,10 +1,10 @@
 using BroStreamerTools;
 
-public class CPHInline
+public partial class CPHInline
 {
     public bool Execute()
     {
-        CPH.TryGetArg("brbDuration", out int brbDuration);
+        CPH.TryGetArg("brbDuration", out string brbDuration);
         CPH.LogInfo(typeof(BRBManager).Assembly.FullName);
         BRBManager.Start(CPH, brbDuration);
         return true;

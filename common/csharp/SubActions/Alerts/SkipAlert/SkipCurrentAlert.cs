a7b9c0b7044765785ed22d299dcdee7be6b78fc8
Loïc Villet <villet.loic@gmail.com>
2026-08-09 10:35:56 +0200

    feat(common, c#): add skip alert action

---
+++ common/csharp/SubActions/Alerts/SkipAlert/SkipCurrentAlert.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class CPHInline
+{
+    public bool Execute()
+    {
+        var payload = new { @event = "SkipCurrentAlert" };
+
+        string json = Newtonsoft.Json.JsonConvert.SerializeObject(payload);
+
+        CPH.WebsocketBroadcastJson(json);
+
+        CPH.LogInfo($"[SkipCurrentAlert] dispatched: {json}");
+
+        return true;
+    }
+}
